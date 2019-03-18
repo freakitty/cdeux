bc9e018a067ae8b59cb6ec73d4c22cc23894271a
castano cindy <castano.cindy1@gmail.com>
2019-03-18 01:37:31 +0100

    project page layout

--- js/projets.js
+++ js/projets.js
@@ -1,26 +1,30 @@
+/*jslint es6 */
 /*jslint browser: true*/
 /*global $, jQuery, TweenMax, TimelineMax, Power4*/
 /* jslint expr: true */
+
 
 $(function () {
     "use strict";
 
 /* ============================= BURGER ================================ */
     
-    function getH() {return window.location.hash; }
-    function setH(h) {window.location.hash = h; }
+    function getH() {
+        return window.location.hash;
+    }
+    function setH(h) {window.location.hash = h;}
 
     function tweenreset() {
-        TweenMax.to('#navbar', 0.7, {top: '-100%'});
+        TweenMax.to("#navbar", 0.7, {top: "-100%"});
     }
 
     function toMenu() {
-        TweenMax.to('#navbar', 0.7, {top: '0%'});
+        TweenMax.to("#navbar", 0.7, {top: "0%"});
     }
 
     function toView(route) {
         switch (route) {
-        case '#navbar':
+        case "#navbar":
             setH(route);
             toMenu();
             break;
@@ -49,11 +53,32 @@ $(function () {
     var typewriter = require('typewriter'),
         twSpan = document.getElementById('typewriter'),
         tw = typewriter(twSpan).withAccuracy(95)
-                             .withMinimumSpeed(5)
-                             .withMaximumSpeed(17)
-                             .build();
+            .withMinimumSpeed(5)
+            .withMaximumSpeed(17)
+            .build();
     tw.type('Nos réalisations. Bon voyage ');
 
-    
     toView(getH());
 });
+
+/* ============================= SCROLL ANIMATION ================================ */
+    
+    $(window).scroll(function () {
+        $(".anim .card").each(function () {
+            const position = $(this).offset().top;
+            const scroll = $(window).scrollTop();
+            const windowHeight = $(window).height();
+            if (scroll > position - windowHeight) {
+                $(this).addClass("active");
+            }
+            if (scroll < 100) {
+                $(this).removeClass("active");
+            }
+        });
+    });
+
+var mq = window.matchMedia("(max-width: 480px)");
+  
+    if (mq.matches) {
+         $('.anim').toggleClass('wrapp');
+    }
